d956ca16f81f232d386cb717269cfd9f581062be
Yoshiki0418 <c1206618@st.kanazawa-it.ac.jp>
2024-08-22 12:48:48 +0900

    モデルのパラメータ計算

---
+++ src/flops_calculator.py
@@ -0,0 +1,44 @@
+import torch
+from torch import nn
+from models import *
+
+def calculate_flops(model, input_size):
+    flops = 0
+    def conv2d_hook(self, input, output):
+        # Calculate FLOPs for convolution
+        batch_size, in_channels, h, w = input[0].size()
+        out_channels, _, kernel_h, kernel_w = self.weight.size()
+        flops_per_instance = out_channels * in_channels * kernel_h * kernel_w * h * w
+        flops_per_instance = flops_per_instance // self.groups
+        flops_total = flops_per_instance * batch_size
+        nonlocal flops
+        flops += flops_total
+
+    hooks = []
+    for layer in model.modules():
+        if isinstance(layer, nn.Conv2d):
+            hooks.append(layer.register_forward_hook(conv2d_hook))
+
+    # Forward pass to trigger hooks
+    input = torch.randn(*input_size)
+    model(input)
+
+    # Remove all hooks
+    for hook in hooks:
+        hook.remove()
+
+    return flops
+
+
+# モデルのインスタンス化
+model = ESPCN4x()
+# パラメータ数の計算
+total_params = sum(p.numel() for p in model.parameters())
+print(f"パラメータ数:{total_params}")
+# 入力サイズを指定 (バッチサイズ, チャンネル数, 高さ, 幅)
+input_size = (1, 3, 64, 64)
+
+# FLOPsを計算
+total_flops = calculate_flops(model, input_size)
+
+print(f"計算量：{total_flops}")
--- src/models/standard/ESPCN.py
+++ src/models/standard/ESPCN.py
@@ -18,7 +18,7 @@ class ESPCN4x(nn.Module):
     def __init__(self) -> None:
         super().__init__()
         self.scale = 4
-        self.conv_1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=5, padding=2)
+        self.conv_1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=5, padding=2)
         nn.init.normal_(self.conv_1.weight, mean=0, std=0.001)
         nn.init.zeros_(self.conv_1.bias)
 
@@ -32,19 +32,17 @@ class ESPCN4x(nn.Module):
         nn.init.normal_(self.conv_3.weight, mean=0, std=0.001)
         nn.init.zeros_(self.conv_3.bias)
 
-        self.conv_4 = nn.Conv2d(in_channels=32, out_channels=(1 * self.scale * self.scale), kernel_size=3, padding=1)
+        self.conv_4 = nn.Conv2d(in_channels=32, out_channels=(3 * self.scale * self.scale), kernel_size=3, padding=1)
         nn.init.normal_(self.conv_4.weight, mean=0, std=0.001)
         nn.init.zeros_(self.conv_4.bias)
 
         self.pixel_shuffle = nn.PixelShuffle(self.scale)
 
     def forward(self, X_in: tensor) -> tensor:
-        X = X_in.reshape(-1, 1, X_in.shape[-2], X_in.shape[-1])
-        X = self.act(self.conv_1(X))
+        X = self.act(self.conv_1(X_in))
         X = self.act(self.conv_2(X))
         X = self.act(self.conv_3(X))
         X = self.conv_4(X)
         X = self.pixel_shuffle(X)
-        X = X.reshape(-1, 3, X.shape[-2], X.shape[-1])
         X_out = clip(X, 0.0, 1.0)
         return X_out
