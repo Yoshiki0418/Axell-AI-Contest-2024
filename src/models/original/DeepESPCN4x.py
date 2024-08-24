4f0db4dcd0e3b43a560a8eeb22fffa218545218d
Yoshiki0418 <c1206618@st.kanazawa-it.ac.jp>
2024-08-24 06:18:11 +0000

    Google colab

--- src/models/original/DeepESPCN4x.py
+++ src/models/original/DeepESPCN4x.py
@@ -15,26 +15,31 @@ class DeepESPCN4x(nn.Module):
         nn.init.normal_(self.conv_2.weight, mean=0, std=0.001)
         nn.init.zeros_(self.conv_2.bias)
         
-        self.conv_3 = nn.Conv2d(in_channels=128, out_channels=32, kernel_size=3, padding=1)
+        self.conv_3 = nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, padding=1)
         nn.init.normal_(self.conv_3.weight, mean=0, std=0.001)
         nn.init.zeros_(self.conv_3.bias)
 
-        self.conv_4 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1)
+        self.conv_4 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, padding=1)
         nn.init.normal_(self.conv_4.weight, mean=0, std=0.001)
         nn.init.zeros_(self.conv_4.bias)
 
-        self.conv_5 = nn.Conv2d(in_channels=32, out_channels=(3 * self.scale * self.scale), kernel_size=3, padding=1)
+        self.conv_5 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1)
         nn.init.normal_(self.conv_5.weight, mean=0, std=0.001)
         nn.init.zeros_(self.conv_5.bias)
+
+        self.conv_6 = nn.Conv2d(in_channels=32, out_channels=(3 * self.scale * self.scale), kernel_size=3, padding=1)
+        nn.init.normal_(self.conv_6.weight, mean=0, std=0.001)
+        nn.init.zeros_(self.conv_6.bias)
 
         self.pixel_shuffle = nn.PixelShuffle(self.scale)
 
     def forward(self, X_in: tensor) -> tensor:
-        X = self.act(self.conv_1(X_in))
-        X = self.act(self.conv_2(X))
-        X = self.act(self.conv_3(X))
+        X1 = self.act(self.conv_1(X_in))
+        X = self.act(self.conv_2(X1))
+        X = self.act(self.conv_3(X)) + X1
         X = self.act(self.conv_4(X))
-        X = self.conv_5(X)
+        X = self.act(self.conv_5(X))
+        X = self.conv_6(X)
         X = self.pixel_shuffle(X)
         X_out = clip(X, 0.0, 1.0)
         return X_out
