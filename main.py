e6d7bc541fe4992d6d07ade07a8171ead05969f5
Yoshiki0418 <c1206618@st.kanazawa-it.ac.jp>
2024-08-25 00:04:13 +0000

    Google colab

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def run(args: DictConfig):
 
     model = ModelClass().to(args.device)
 
-    checkpoint_path = "model_epoch_20.pth"
+    checkpoint_path = "model_epoch_40.pth"
     model.load_state_dict(torch.load(checkpoint_path))
 
     #----------------------------
