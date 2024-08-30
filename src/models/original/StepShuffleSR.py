520c57c8077335af9568074c8e4e788f86b4bc32
Yoshiki0418 <c1206618@st.kanazawa-it.ac.jp>
2024-08-30 10:42:16 +0900

    step model change

--- src/models/original/StepShuffleSR.py
+++ src/models/original/StepShuffleSR.py
@@ -11,27 +11,26 @@ class StepShuffleSR(nn.Module):
         super().__init__()
 
         self.scale = 4
-        self.conv_1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=5, padding=2)
+        self.i = 3
+        self.conv_1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=9, padding=4)
         nn.init.normal_(self.conv_1.weight, mean=0, std=0.001)
         nn.init.zeros_(self.conv_1.bias)
 
         self.act = nn.ReLU()
 
-        self.conv_2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1)
-        nn.init.normal_(self.conv_2.weight, mean=0, std=0.001)
-        nn.init.zeros_(self.conv_2.bias)
-        
-        self.conv_3 = nn.Conv2d(in_channels=64, out_channels=16, kernel_size=3, padding=1)
-        nn.init.normal_(self.conv_3.weight, mean=0, std=0.001)
-        nn.init.zeros_(self.conv_3.bias)
-
-        self.conv_4 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, padding=1)
-        nn.init.normal_(self.conv_4.weight, mean=0, std=0.001)
-        nn.init.zeros_(self.conv_4.bias)
+        self.residual_block = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1),
+        )
+        nn.init.normal_(self.residual_block[0].weight, mean=0, std=0.001)
+        nn.init.zeros_(self.residual_block[0].bias)
+        nn.init.normal_(self.residual_block[2].weight, mean=0, std=0.001)
+        nn.init.zeros_(self.residual_block[2].bias)
 
         # Upsampling layers
         self.upsampling_layer1 = nn.Sequential(
-            nn.Conv2d(in_channels=16, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1),
             nn.PixelShuffle(2),
             nn.PReLU(),
         )
@@ -39,7 +38,7 @@ class StepShuffleSR(nn.Module):
         nn.init.zeros_(self.upsampling_layer1[0].bias)
 
         self.upsampling_layer2 = nn.Sequential(
-            nn.Conv2d(in_channels=16, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=8, out_channels=32, kernel_size=3, stride=1, padding=1),
             nn.PixelShuffle(2),
             nn.PReLU(),
         )
@@ -47,15 +46,16 @@ class StepShuffleSR(nn.Module):
         nn.init.zeros_(self.upsampling_layer2[0].bias)
 
         # Final Convolution
-        self.final_conv = nn.Conv2d(in_channels=16, out_channels=3, kernel_size=5, stride=1, padding=2)
+        self.final_conv = nn.Conv2d(in_channels=8, out_channels=3, kernel_size=5, stride=1, padding=2)
         nn.init.normal_(self.final_conv.weight, mean=0, std=0.001)
         nn.init.zeros_(self.final_conv.bias)
 
     def forward(self, X_in: torch.Tensor) -> torch.Tensor:
-        X1 = self.act(self.conv_1(X_in))
-        X = self.act(self.conv_2(X1)) + X1
-        X2 = self.act(self.conv_3(X))
-        X = self.act(self.conv_4(X2)) + X2
+        X = self.act(self.conv_1(X_in))
+
+        for _ in range(self.i):
+            X = self.residual_block(X) + X
+        
         X = self.upsampling_layer1(X) 
         X = self.upsampling_layer2(X)
         X = self.final_conv(X)
